da8d82f854bd3073198b1d986417a53ae91e02bd
PEEVEEz <peeveezz2020@gmail.com>
2022-02-06 22:19:52 +0200

    s

--- core/src/client/events/main.lua
+++ core/src/client/events/main.lua
@@ -2,5 +2,5 @@
 RegisterNetEvent('core:updateData', function(lobby, data)
     if not lobby then Core.Lobbys = data
     else Core.Lobbys[lobby] = data end
-    print("Data updated1")
+    print("Data updated12")
 end)
